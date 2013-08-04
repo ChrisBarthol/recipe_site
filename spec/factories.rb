ef9032bf1eb8c43b84145443631b95bfb8ace38f
Chris Barthol <Chris.Barthol@gmail.com>
2013-08-03 21:10:44 -0400

    Finish user edit, update, indexm and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,21 @@
+#FactoryGirl.define do
+#	factory :user do
+#		name		"Chris Barthol"
+#		email		"chris.barthol@gmail.com"
+#		password	"foobar"
+#		password_confirmation	"foobar"
+#	end
+#end
+
 FactoryGirl.define do
-	factory :user do
-		name		"Chris Barthol"
-		email		"chris.barthol@gmail.com"
-		password	"foobar"
-		password_confirmation	"foobar"
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
+  end
 end
