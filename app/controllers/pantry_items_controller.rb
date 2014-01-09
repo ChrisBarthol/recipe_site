aabf61c1a351a4d75fe49ef8ad6a3ec286236957
ChrisBarthol <Chris.Barthol@gmail.com>
2014-01-09 13:56:34 -0500

    Pantry_item saves only last ingredient

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,12 +1,19 @@
-class PantryItemController < ApplicationController
-	before_action :sign_in_user
+class PantryItemsController < ApplicationController
+	before_action :signed_in_user
+
+	def new
+		@ingredient = Ingredient.find(params[:ingredient_id])
+		@user = User.find(params[:id])
+		@pantry_item = Array.new(3) { @user.pantry_items.build }
+	end
+
 
 	def create
-		#@recipe =
-
-		@pantry_item = User.find(params[:ingredient][:ingredient_id])
-		current_user.save_ingredient!(@pantry_item)
-		redirect_to @recipe
+		@ingredient = Ingredient.find(params[:ingredient_id])
+		@recipe = Recipe.find_by_id(@ingredient.recipe_id)
+		@pantry_item = current_user.pantry_items.build(ingredient_id: @ingredient.id)
+		current_user.save_ingredient!(@ingredient)
+		redirect_to recipe_path(@recipe)
 	end
 
 	def destroy
