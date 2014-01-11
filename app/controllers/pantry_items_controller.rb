669b431def1b7d504721fb00a176c649cbadee3e
ChrisBarthol <Chris.Barthol@gmail.com>
2014-01-11 17:29:44 -0500

    Cleaned up a few lines in pantry items

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -2,9 +2,7 @@ class PantryItemsController < ApplicationController
 	before_action :signed_in_user
 
 	def new
-		@ingredient = Ingredient.find(params[:ingredient_id])
-		@user = User.find(params[:id])
-		@pantry_item = Array.new(3) { @user.pantry_items.build }
+
 	end
 
 
@@ -15,10 +13,6 @@ class PantryItemsController < ApplicationController
 			current_user.save_ingredient!(ingredient)	
 		end
 
-		#@ingredient = Ingredient.find(params[:ingredient_id])
-		#@recipe = Recipe.find_by_id(@ingredient.recipe_id)
-		#@pantry_item = current_user.pantry_items.build(ingredient_id: @ingredient.id)
-		#current_user.save_ingredient!(@ingredient)
 		redirect_to recipe_path(@recipe)
 	end
 
