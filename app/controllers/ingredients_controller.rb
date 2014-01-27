1bebac231b51d3bb152c258b8068b5796e33ec8f
ChrisBarthol <Chris.Barthol@gmail.com>
2014-01-27 17:48:26 -0500

    save individual ingredients to pantry

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,7 +7,10 @@ class IngredientsController < ApplicationController
 
 	def create
 		@ingredient = Ingredient.new(ingred_params)
+		@id = @ingredient.id
+
 		if @ingredient.save
+		 current_user.save_ingredient!(@ingredient)
 		 redirect_to pantry_user_path(current_user), :flash => {notice: "Ingredient Added!" }
 		else
 		 redirect_to pantry_user_path(current_user), :flash => {error: "Error: Failed to save" }
@@ -20,7 +23,7 @@ class IngredientsController < ApplicationController
 	private
 
 		def ingred_params
-			params.require(:ingredient).permit(:name, :quantity, :unit, :id, :style, :create_at, :updated_at)
+			params.permit(:name, :quantity, :unit, :id, :style, :create_at, :updated_at)
 		end
 	
 end
