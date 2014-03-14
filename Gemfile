1b7132c071f9ceddc0aba500df6f41c62f8fdff9
ChrisBarthol <Chris.Barthol@gmail.com>
2014-03-14 12:03:28 -0400

    added calendar select for expiration

--- Gemfile
+++ Gemfile
@@ -55,4 +55,5 @@ group :assets do
   gem 'sass-rails',   "4.0.0"
   gem 'coffee-rails', "4.0.0"
   gem 'uglifier', '2.1.1'
+  gem 'jquery-ui-rails'
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require jquery.ui.datepicker
 //= require bootstrap
 //= require_tree .
 
